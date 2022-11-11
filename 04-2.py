96bfab468499dd3046c580b87c0136280559ffd0
JULee <oiseau1038c@gmail.com>
2022-11-12 08:39:22 +0900

    4장 CSV파일 갖고 놀기 지삭

---
+++ 04-2.py
@@ -0,0 +1,15 @@
+# 4장 4~
+
+# 04-4 CSV 파일 데이터 분석
+print('# 04-4 CSV 파일 데이터 분석')
+
+# Do it 외국인 비율이 3% 넘는 구 정보만 CSV 파일로 저장하기
+print('# Do it 외국인 비율이 3% 넘는 구 정보만 CSV 파일로 저장하기')
+
+import os, re
+from Function import Learning, usercsv
+
+path = Learning.MoveLearningDataDir()
+print(path)
+
+
