55f8c9f7d5fd0463d8e3299a47ebd1d25e696ead
JULee <oiseau1038c@gmail.com>
2022-12-05 15:49:31 +0900

    Pandas 연습

---
+++ 5장/05-6.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import re, os, sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from Function import usercsv, Learning
+
+path = Learning.MoveLearningDataDir()
+print(path)
+
