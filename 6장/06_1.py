556d06a30bfaeafe0afb1b8df7df3236798d28c7
JULee <oiseau1038c@gmail.com>
2023-02-18 15:46:49 +0900

    크롤링 셀리늄 슬쩍 봄

--- 6장/06_1.py
+++ 6장/06_1.py
@@ -2,3 +2,9 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from Function import usercsv, Learning
 import numpy as np 
+import requests
+import urllib.request as ur
+from bs4 import BeautifulSoup as bs
+
+url = 'https://quotes.toscrape.com'
+html = ur.urlopen(url)
