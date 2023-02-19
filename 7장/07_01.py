58a391da29fa5a154297299310b9116b76b333e4
JULee <oiseau1038c@gmail.com>
2023-02-20 08:40:10 +0900

    selenium python 실행해 보기 01

---
+++ 7장/07_01.py
@@ -0,0 +1,17 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from Function import usercsv, Learning
+import numpy as np 
+import requests
+import urllib.request as ur
+from bs4 import BeautifulSoup as bs
+
+# Selenuum 임포트
+from selenium import webdriver as selen
+
+driver = selen.Chrome()
+#url = 'https://www.google.com'
+#url = r'https://prod.danawa.com/list/?cate=112747'
+url = r'https://news.daum.net/'
+driver.get(url)
+# 인터넷 창을 연다.
